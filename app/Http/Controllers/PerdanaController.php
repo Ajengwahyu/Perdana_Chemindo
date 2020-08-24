18361430f24d9d774748d8029593799f69cc40e2
Ajengwahyu <Ajengwahyu>
2020-08-24 13:16:03 +0700

    full

---
+++ app/Http/Controllers/PerdanaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use App\User;
+
+class PerdanaController extends Controller
+{
+    //
+}
---
+++ resources/views/perdana/perdana.blade.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><link href="https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&family=Roboto&display=swap" rel="stylesheet">
+        <title>PERDANA</title>
+        <link rel="stylesheet" href="{{('css/perdana.css')}}">
+    </head>
+    <body>
+        <!-- HANALDA -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <img class="imgg1" src="img/lg.png">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav" >
+                    <ul class="navbar-nav" style="margin-top: -35px; font-size: 20px; margin-left: 300px;">
+                        <!-- <li class="nav-item">
+                            <a class="navbar-brand" href="#" style="font-weight: bold; font-size: 12px;"></a>
+                        </li>
+                        <li class="nav-item">
+                             <a class="navbar-brand" href="#" style="font-weight: bold; font-size: 12px;"></a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="#" style="font-family: 'Roboto', sans-serif; font-weight: 700; font-size: 12px; margin-left: 250px;"> WhatsApp<img class="imgg2" src="img/wa.png"> 0812 46666 968</a>
+                        </li>
+                    </ul>
+
+                    <ul class="navbar-nav" style="font-family: 'Fira Sans', sans-serif; font-weight: 500; margin-top: 30px; margin-left: -637px;">
+                        <li class="nav-item">
+                            <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">Industry Application</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">Quality</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="navbar-brand nav-link" href="#" style=" font-size: 18px;">Contact</a>
+                        </li>
+                    </ul>
+              </div>
+            </div>
+        </nav>        
+        <div class="konten">
+            <div class="container">
+                <h5 class="ctn" style="font-family: 'Fira Sans', sans-serif; padding-top: 180px; padding-bottom: 200px; color: white; font-weight: bold;">
+                    <p style="font-size: 33px;" class="t1">Your partner of chemicals and <br>
+                        fiberglass composite materials <br>
+                    <br></p>
+                    <p style="font-size: 17px; font-weight: 300; margin-top:-30px;" class="t2">
+                        Perdana Chemindo Perkasa id specialised in <br>
+                        supplying the best source of waste water and water <br>
+                        treatment chemicalsm general industrial chemicals, <br>
+                        fiberglass composite materials and fiberglass <br>
+                        project fabrications. <br> <br>
+                    </p> 
+                    <div class="btn-group dropright" style="background-color: rgb(215, 215, 241);">
+                      <button type="button" class="btn" aria-haspopup="true" aria-expanded="false">
+                        LEARN MORE
+                      </button>
+                    </div>
+                </h5>
+            </div>
+        </div>
+
+        <!-- NETA -->
+        <div class="top-content">
+            <h2><b>Perdana Chemindo Perkasa Products & Services</b></h2>
+            <ul>
+                <li>
+                    <img src="img/top4.png" alt="">
+                    <p>Wastewater & water<br>Treatment Chemicals</p>
+                </li>
+                <li>
+                    <img src="img/top1.png" alt="">
+                    <p>Fiberglass Composite <br> Material</p>
+                </li>
+                <li>
+                    <img src="img/top2.png" alt="">
+                    <p>General Industrial <br> Chemical</p>
+                </li>
+                <li>
+                    <img src="img/top3.png" alt="">
+                    <p>Custom Fiberglass Project <br> Fabrication</p>
+                </li>
+            </ul>
+        </div>
+
+        <div class="btm-content">
+            <h2 class="title"><b>Industry Application For Our Products</b></h2>
+            <ul>
+                <li>
+                    <img src="img/i1.png" alt="">
+                    <p>Water Treatment</p>
+                </li>
+                <li>
+                    <img src="img/i2.png" alt="">
+                    <p>Marine</p>
+                </li>
+                <li>
+                    <img src="img/i3.png" alt="">
+                    <p>Household</p>
+                </li>
+                <li>
+                    <img src="img/i4.png" alt="">
+                    <p>Industrial</p>
+                </li>
+                <li>
+                    <img src="img/i5.png" alt="">
+                    <p>Automotive</p>
+                </li>
+                <li>
+                    <img src="img/i6.png" alt="">
+                    <p>Construction</p>
+                </li>
+            </ul>
+            <div class="link" style="margin-top: -70px;">
+                <a href="#">Click Here To View All Industry Application</a>
+            </div>
+        </div>
+
+        <!-- AJENG -->
+        <section class="bottoms-header">
+            <div class="info">
+                <h4>
+                    We are the secret behind your <br> successful products
+                </h4>
+                <h5>
+                    We make sure that our customers' demands <br>
+                    are met by providing the highest chemical <br>
+                    products available. 
+                </h5>
+                <h6>
+                    To bring our customer the best source of their flavours, we work <br>
+                    and comply with the world's best brands and manufacturer of <br>
+                    Natural Extracts, Aromatic Chemicals, Natural Flavours and <br>
+                    Essential Oils. We make sure our customer holds the keys to <br>
+                    limitless possibilities for their taste explorations.
+                </h6>
+                <div class="box">
+                    <a href="#"><b>LEARN MORE</b></a>
+                </div>
+            </div>
+        </section>
+
+        <section class="tops-widget">
+            <div class="title-widget">
+                <h5>Why Perdana Chemindo Perkasa as your first choice for chemicals</h5>
+                <div class="sub-title">
+                    <h3>990+</h3><p>Variations of products <br> for you to choose</p>
+
+                    <h3>20+</h3><p>Years of experience in industrial <br> chemicals & fiberglass fabrication</p>
+
+                    <h3>200+</h3><p>Tons of product delivery <br> every month</p>
+                </div>
+            </div>
+            <div class="cek">
+                <a href="#" class="btn btn-link"><img src="img/chevron.png" alt="" height="40" width="40"><br><p class="text-dark">TOP</p></a>
+            </div>
+        </section>
+
+        <!-- JUNANDA -->
+        <div class="bawah">
+            <div class="line-blue">
+                <h4 class="line-text">OUR QUALITY CERTIFICATIONS</h4>
+            </div>
+            <!-- <div class="container"> -->
+                <div class="logo">
+                    <img class="img-logo" src="img/footer-lg1.png"  alt="">
+                </div>
+            <!-- </div> -->
+            <div class="foter">
+                <div class="fot">
+                    <footer class="page-footer font-small indigo">
+                        <div class="text-center text-md-left">
+                            <div class="row">
+                                <div class="col-md-3 mx-auto">
+                                    <div class="f1">
+                                        <p class=" mt-3 mb-4">Browse Our Site</p>
+                                        <ul class="list-unstyled">
+                                            <li>
+                                                <a>About</a>
+                                            </li>
+                                            <li>
+                                                <a>Industry Application</a>
+                                            </li>
+                                            <li>
+                                                <a>Quality</a>
+                                            </li>
+                                            <li>
+                                                <a>Contact</a>
+                                            </li>
+                                            </ul>
+                                    </div>
+                                </div>
+                                <hr class="clearfix w-100 d-md-none">
+                                <div class="col-md-3 mx-auto">
+                                    <div class="f2">
+                                        <p class="mt-3 mb-4">Products</p>
+                                        <ul class="list-unstyled">
+                                            <li>
+                                                <a>Waste Water & Water Treatment Chemicals</a>
+                                            </li>
+                                            <li>
+                                                <a>Industrial General Chemicals</a>
+                                            </li>
+                                            <li>
+                                                <a>Fiberglass Composite Material</a>
+                                            </li>
+                                            <li>
+                                                <a>Fiberglass Custom Project Fabrication</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <hr class="clearfix w-100 d-md-none">
+                                <div class="col-md-3 mx-auto">
+                                    <div class="f3">
+                                        <p class="mt-3 mb-4">Inquire Now</p>
+                                            <ul class="list-unstyled">
+                                                <li>
+                                                    <a><img src="img/footer-wa.png" alt="" class="fi1"> +62 812 4666 6968</a>
+                                                </li>
+                                                <li>
+                                                    <a><img src="img/footer-telp.png" alt="" class="fi1"> +62 31 5353 668</a>
+                                                </li>
+                                                <li>
+                                                    <a><img src="img/footer-mail.png" alt="" class="fi1"> admin@perdanachemindo.com</a>
+                                                </li>
+                                            </ul>
+                                    </div>
+                                </div>
+                                <hr class="clearfix w-100 d-md-none">
+                                <div class="col-md-3 mx-auto">
+                                    <div class="f4">
+                                        <p class="Tp-18 mt-4 mb-4">PT. Perdana Chemindo Perkasa</p>
+                                        <ul class="ill list-unstyled">
+                                            <li>
+                                                <a>Copyright © 2020,  All Rights Reserved</a>
+                                            </li>
+                                            <li>
+                                                <a>Website by Mark Design.</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
